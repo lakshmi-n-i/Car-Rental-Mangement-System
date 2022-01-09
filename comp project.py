1461645cdb0e852badc274f414c3d60ded6a7cc1
deepiikaa24 <91751091+deepiikaa24@users.noreply.github.com>
2022-01-09 22:53:43 +0530

    Add files via upload
    Added new windows for rentals and returns

--- comp project.py
+++ comp project.py
@@ -11,17 +11,26 @@ root.title("Car Rental Management System")
 root.geometry("600x500")
 headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
 headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
-headingLabel = Label(headingFrame1, text="Welcome to bon voyage rentals", bg='black', fg='white', font=('Courier',15))
+headingLabel = Label(headingFrame1, text="Welcome to bon voyage rentals", bg='black', fg='white', font=('Courier',30))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
-def clicked():
-    lb2=Label(root,text="select car").grid(row=5,column=2)
-    lb3=Label(root,text="return a car").grid(row=7,column=2)
+def rent():
+    rentwindow=Toplevel(root)
+    rentwindow.title("Car Rentals")
+    rentwindow.geometry("200x200")
+    lb2=Label(rentwindow,text="select car",bg="black",fg="white",font=("Courier",20,'bold')).pack()
 
 
-btn1=Button(root,text="rent a car",command=clicked,bg="black",fg='white',font=('arial',20,'bold'))
+btn1=Button(root,text="rent a car",command=rent,bg="black",fg='white',font=('arial',15,'bold'))
 btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
-btn2=Button(root,text="return a car",command=clicked,bg="black",fg='white',font=('arial',20,'bold'))
+
+def returns():
+
+    returnwindow=Toplevel(root)
+    returnwindow.title("Car Returns")
+    returnwindow.geometry("200x200")
+    lb3=Label(returnwindow,text="return a car",bg="black",fg="white",font=("Courier",20,'bold')).pack()
+btn2=Button(root,text="return a car",command=returns,bg="black",fg='white',font=('arial',15,'bold'))
 btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
 
 
