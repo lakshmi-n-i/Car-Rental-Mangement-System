8bc8d70733654b7e121ccec0017ea0cf22d40d4b
deepiikaa24 <91751091+deepiikaa24@users.noreply.github.com>
2022-01-11 23:31:03 +0530

    Add files via upload
    merged version till now

--- comp project.py
+++ comp project.py
@@ -1,7 +1,6 @@
-#WE HAVE TO ADD DESCRIPTION FOR EACH CAR
+#with shortened code
 from tkinter import*
 from tkinter import ttk
-from tkinter.font import BOLD
 import mysql.connector
 connection=mysql.connector.connect(host='localhost',user='root',password='munich24',database='computerproject')
 cursor=connection.cursor()
@@ -24,60 +23,38 @@ def screen1():
 
 
 def rent():
-    #root.destroy()
+    root.destroy()
     global screen2
     screen2=Tk()
-    screen2.geometry("800x800")
+    screen2.geometry("1200x800")
     screen2.title("Car Rentals")
-    lb2=Label(screen2,text="Please fill in your specifications",bg="black",fg="white",font=("Segoe Print",20,'bold'))
-    lb2.place(relx=0.35,rely=0.01)
-    lb3=Label(screen2,text="Seater capacty",bg="black", fg="white",font=("Arial",15,'bold'))
-    lb3.place(relx=0.03, rely=0.1)
-    global var1
-    global var2
-    global var3
-    var1=IntVar()
-    var2=IntVar()
-    var3=IntVar()
-    var4=IntVar()
-    var5=IntVar()
-    var6=IntVar()
-    var7=IntVar()
-    var8=IntVar()
-    var9=IntVar()
-    var10=IntVar()
-    var11=IntVar()
-    cb1= Checkbutton(screen2,text="5-seater", variable=var1,font=('Calibri',14,'bold'))
-    cb2= Checkbutton(screen2,text="7-seater", variable=var2,font=('Calibri',14,'bold'))
-    cb3= Checkbutton(screen2,text="8-seater", variable=var3,font=('Calibri',14,'bold'))
+    lb2=Label(screen2,text="Please fill in your specifications",bg="black",fg="white",font=("Segoe Print",20,'bold')).place(relx=0.33,rely=0.01)
+    lb3=Label(screen2,text="Seater capacty",bg="black", fg="white",font=("Arial",15,'bold')).place(relx=0.03, rely=0.08)
+    global var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11
+    var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11 =IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar(),IntVar()
+    cb1= Checkbutton(screen2,text="5-seater", variable=var1,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.12)
+    cb2= Checkbutton(screen2,text="7-seater", variable=var2,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.16)
+    cb3= Checkbutton(screen2,text="8-seater", variable=var3,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.20)
     '''
     cb1.deselect()
     cb2.deselect()
     cb3.deselect()
     '''
-    cb1.place(relx=0.03, rely=0.14)
-    cb2.place(relx=0.03, rely=0.18)
-    cb3.place(relx=0.03, rely=0.22)
-    lb4=Label(screen2,text="Pick-up Location",bg="black", fg="white",font=("Arial",15,'bold'))
-    lb4.place(relx=0.03, rely=0.3)
-    cb4= Checkbutton(screen2,text="Bellandur", variable=var4,font=('Calibri',14,'bold'))
-    cb5= Checkbutton(screen2,text="Shivajinagar", variable=var5,font=('Calibri',14,'bold'))
-    cb6= Checkbutton(screen2,text="Koramangala", variable=var6,font=('Calibri',14,'bold'))
-    cb7= Checkbutton(screen2,text="Banashankari", variable=var7,font=('Calibri',14,'bold'))
-    cb8= Checkbutton(screen2,text="Indiranagar", variable=var8,font=('Calibri',14,'bold'))
-    cb9= Checkbutton(screen2,text="Hsr Layout", variable=var9,font=('Calibri',14,'bold'))
-    cb4.place(relx=0.03, rely=0.34)
-    cb5.place(relx=0.03, rely=0.38)
-    cb6.place(relx=0.03, rely=0.42)
-    cb7.place(relx=0.03, rely=0.46)
-    cb8.place(relx=0.03, rely=0.5)
-    cb9.place(relx=0.03, rely=0.54)
-    lb5=Label(screen2,text="Driver Needed?",bg="black", fg="white",font=("Arial",15,'bold'))
-    lb5.place(relx=0.03, rely=0.62)
-    cb10= Checkbutton(screen2,text="Yes", variable=var10,font=('Calibri',14,'bold'))
-    cb11= Checkbutton(screen2,text="No", variable=var11,font=('Calibri',14,'bold'))
-    cb10.place(relx=0.03, rely=0.66)
-    cb11.place(relx=0.03, rely=0.7)
+    
+    lb4=Label(screen2,text="Pick-up Location",bg="black", fg="white",font=("Arial",15,'bold')).place(relx=0.03, rely=0.28)
+    cb4= Checkbutton(screen2,text="Bellandur", variable=var4,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.32)
+    cb5= Checkbutton(screen2,text="Shivajinagar", variable=var5,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.36)
+    cb6= Checkbutton(screen2,text="Koramangala", variable=var6,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.40)
+    cb7= Checkbutton(screen2,text="Banashankari", variable=var7,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.44)
+    cb8= Checkbutton(screen2,text="Indiranagar", variable=var8,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.48)
+    cb9= Checkbutton(screen2,text="Hsr Layout", variable=var9,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.52)
+    lb5=Label(screen2,text="Driver Needed?",bg="black", fg="white",font=("Arial",15,'bold')).place(relx=0.03, rely=0.59)
+    cb10= Checkbutton(screen2,text="Yes", variable=var10,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.63)
+    cb11= Checkbutton(screen2,text="No", variable=var11,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.67)
+    lb6=Label(screen2,text="Price Range",bg="black", fg="white",font=("Arial",15,'bold')).place(relx=0.03, rely=0.74)
+    cb12= Checkbutton(screen2,text="2000-3000", variable=var4,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.78)
+    cb13= Checkbutton(screen2,text="3000-4000", variable=var5,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.82)
+    cb14= Checkbutton(screen2,text="4000-5000", variable=var6,font=('Calibri',14,'bold')).place(relx=0.03, rely=0.86)
     btn3= Button(screen2, text= "OK",font=('Calibri',15)).place(relx=0.9,rely=0.9)
     screen2.mainloop()
 
@@ -90,7 +67,7 @@ def returns():
 
     def show():
         mylabel=Label(screen3,text= "You returned a "+cars.get()+" at "+area.get() ,font=('Calibri',20,'bold'),fg="white",bg="black").place(relx=0.32,rely=0.7)
-       
+
     cars=StringVar()
     cars.set("WagonR")
     area=StringVar()
@@ -103,15 +80,8 @@ def returns():
 
     e=Entry(screen3,width=30).place(relx=0.30,rely=0.91)
     lb17=Label(screen3,text="Help us improve!",font=('Arial',14,'bold')).place(relx=0.15,rely=0.9)
-    
-
-
-
-
 
     screen3.mainloop()
-
-
 
 
 screen1()
